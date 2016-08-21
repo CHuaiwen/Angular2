0440290e2f87164b64ac16fe95522b11693d5acd
CHuaiWen <nick.chang665@gmail.com>
2016-08-21 16:17:29 +0800

    10 練習 Angular 2 範本參考變數的應用方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   keyword = 'Test';
 
-  doSearch(inputDOM: HTMLInputElement) {
-      this.keyword = inputDOM.value;
+  doSearch(keyword: string) {
+      this.keyword = keyword;
   }
 }
