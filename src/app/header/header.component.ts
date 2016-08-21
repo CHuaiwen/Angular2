eb0919a96d1d7f5927478d8cdd60f461e06e0178
CHuaiWen <nick.chang665@gmail.com>
2016-08-21 15:19:31 +0800

    05 練習 Angular 2 資料繫結：內嵌繫結、屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title = 'The Will Will Web!';
+  titleurl = 'http://blog.miniasp.com/';
+  subtitle = '<b>記載著 Will 在網路世界的學習心得與技術分享</b>';
   constructor() { }
 
   ngOnInit() {
