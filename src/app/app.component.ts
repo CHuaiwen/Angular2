48ce23855aefc5b4bcc389914a54d1ae281fa60c
CHuaiWen <nick.chang665@gmail.com>
2016-08-21 16:00:38 +0800

    08 隨堂測驗：在搜尋輸入框打字後，按下 Enter 鍵才會寫入 keyword 屬性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   keyword = '';
+  doSearch($event: KeyboardEvent) {
+    console.log($event);
+    //.target代表$event的主體標籤(節點)，是一個HTMLInputElement
+    let input = $event.target as HTMLInputElement;
+    //keyCode==13代表Enter鍵
+    if ($event.keyCode == 13){
+        this.keyword = input.value;
+    }
+  }
 }
