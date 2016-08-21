2e612c8e59a68091abe35c1294b1951279ecf6b8
CHuaiWen <nick.chang665@gmail.com>
2016-08-21 15:41:37 +0800

    06 練習 Angular 2 資料繫結：事件繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,9 +9,18 @@ export class HeaderComponent implements OnInit {
   title = 'The Will Will Web!';
   titleurl = 'http://blog.miniasp.com/';
   subtitle = '<b>記載著 Will 在網路世界的學習心得與技術分享</b>';
+  num = 0;
   constructor() { }
 
   ngOnInit() {
   }
-
+  AddNum($event: MouseEvent) {
+    console.log($event);
+    //如果按住Alt再click事件則num-1
+    if ($event.altKey) {
+      this.num -= 1;
+    } else {
+      this.num += 1;
+    }
+  }
 }
